a0bb75b464055fccee18470c6efeaffb9498b558
SURESH VJ <vijjanaramasuresh@gmail.com>
2024-10-27 13:05:32 +0530

    Readme updated

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Ecomers_product_detection_from_text_comment
+# Ecomers Product Detection From Text Comment
+
+## 1. How to Run the Code:
+> Tere are two runs need to get te result.
+> 1. Run the jupyter notebook code to train the model
+> 2. Run the app.py file to use the product.
+
+## 2. Instalations and Setup:
+**Install Streamlit:** Run the following commands to uninstall and reinstall streamlit to ensure you have the latest version.
+
+> pip uninstall streamlit  
+> pip install streamlit
+
+**Verify Installation:** After reinstalling, verify that streamlit is installed correctly by running:
+
+> streamlit --version
+
+## 3. How to Run App:
+**Run the App with Full Path:** Run below two commands to run the app:
+
+> python -u "e:\NLP\Ecomers_product_detection_from_text_comment\app.py"  
+> python -m streamlit run e:\NLP\Ecomers_product_detection_from_text_comment\app.py
--- app.py
+++ app.py
@@ -7,18 +7,21 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 #loading models
-clf = pickle.load(open('clf.pkl','rb'))
-tfidfd = pickle.load(open('tfidf.pkl','rb'))
+ovr_knn = pickle.load(open('results/ovr_knn.pkl','rb'))
+tfidfd = pickle.load(open('results/tfidf.pkl','rb'))
 
-def clean_resume(resume_text):
-    clean_text = re.sub('http\S+\s*', ' ', resume_text)
-    clean_text = re.sub('RT|cc', ' ', clean_text)
-    clean_text = re.sub('#\S+', '', clean_text)
-    clean_text = re.sub('@\S+', '  ', clean_text)
-    clean_text = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', clean_text)
-    clean_text = re.sub(r'[^\x00-\x7f]', r' ', clean_text)
-    clean_text = re.sub('\s+', ' ', clean_text)
-    return clean_text
+# Map category ID to category name
+category_mapping = {'Books': 0, 'Clothing & Accessories': 1, 'Electronics': 2, 'Household': 3}
+
+def data_cleaning(text):
+    
+    # Step 1: Remove non-alphanumeric characters
+    cleaned_text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
+    # Step 2: Remove extra spaces
+    cleaned_text = re.sub(r"\s+", " ", cleaned_text).strip()
+    
+    return cleaned_text.lower()
+
 # web app
 def main():
     st.title("Resume Screening App")
@@ -32,39 +35,10 @@ def main():
             # If UTF-8 decoding fails, try decoding with 'latin-1'
             resume_text = resume_bytes.decode('latin-1')
 
-        cleaned_resume = clean_resume(resume_text)
+        cleaned_resume = data_cleaning(resume_text)
         input_features = tfidfd.transform([cleaned_resume])
-        prediction_id = clf.predict(input_features)[0]
+        prediction_id = ovr_knn.predict(input_features)[0]
         st.write(prediction_id)
-
-        # Map category ID to category name
-        category_mapping = {
-            15: "Java Developer",
-            23: "Testing",
-            8: "DevOps Engineer",
-            20: "Python Developer",
-            24: "Web Designing",
-            12: "HR",
-            13: "Hadoop",
-            3: "Blockchain",
-            10: "ETL Developer",
-            18: "Operations Manager",
-            6: "Data Science",
-            22: "Sales",
-            16: "Mechanical Engineer",
-            1: "Arts",
-            7: "Database",
-            11: "Electrical Engineering",
-            14: "Health and fitness",
-            19: "PMO",
-            4: "Business Analyst",
-            9: "DotNet Developer",
-            2: "Automation Testing",
-            17: "Network Security Engineer",
-            21: "SAP Developer",
-            5: "Civil Engineer",
-            0: "Advocate",
-        }
 
         category_name = category_mapping.get(prediction_id, "Unknown")
 
